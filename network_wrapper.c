b3228fa89be66d25a38fc598bd20cca409ebbf6b
leomanangka <leomanangka@outlook.com>
2025-05-16 14:48:47 +0700

    fix typo and declare variable

--- network_wrapper.c
+++ network_wrapper.c
@@ -14,7 +14,7 @@
 #define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_HASH_TO_ELEMENT
 #define ESP_H2E_IDENTIFIER CONFIG_ESP_WIFI_PW_ID
 #elif CONFIG_ESP_WPA3_SAE_PWE_BOTH
-#define ESP_WIFI_SAE_MODE WP3_SAE_PWE_BOTH
+#define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_BOTH
 #define ESP_H2E_IDENTIFIER CONFIG_ESP_WIFI_PW_ID
 #endif
 
@@ -135,7 +135,7 @@ void wifi_start(void) {
   ESP_ERROR_CHECK(esp_event_loop_create_default());
 
   /* Initialize NVS */
-  esp_err_t = nvs_flash_init();
+  esp_err_t ret = nvs_flash_init();
   if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
       ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
     ESP_ERROR_CHECK(nvs_flash_erase());
